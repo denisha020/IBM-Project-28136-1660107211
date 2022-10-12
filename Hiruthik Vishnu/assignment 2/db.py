11388cf42e8bf2e514da653611f5b6c050fdfd62
Vishnu0753 <87539698+Vishnu0753@users.noreply.github.com>
2022-10-12 22:48:50 +0530

    Delete db.py

--- Hiruthik Vishnu/assignment 2/db.py
+++
@@ -1,3 +0,0 @@
-import ibm_db
-conn = ibm_db.connect("DATABASE=bludb;HOSTNAME=fbd88901-ebdb-4a4f-a32e-9822b9fb237b.c1ogj3sd0tgtu0lqde00.databases.appdomain.cloud;PORT=32731;SECURITY=SSL;SSLServerCertificate=DigiCertGlobalRootCA.crt;UID=brd83146;PWD=86sieylZYORbSY3i","","") 
-print(conn)
